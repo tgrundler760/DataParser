034bfc087c941eb9b808eabd4a81f72480074ad7
Toma <tgrundler760@student.fuhsd.org>
2019-03-10 22:17:07 -0700

    Updated main to reflect new method in Utils.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
-        ArrayList<ElectionResult> results = Utils.parse2016ElectionResults(data);
+        String resultsData = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        String educationData = Utils.readFileAsString("data/Education.csv");
+        String unemploymentData = Utils.readFileAsString("data/Unemployment.csv");
+        DataManager results = Utils.parse2016Data(resultsData, educationData, unemploymentData);
+        if (results.getStates().size() > 48) System.out.println("it worked I think");
+        //ArrayList<ElectionResult> results = Utils.parse2016ElectionResults(data);
     }
 }
